f3d4349037a79cf837222a88c394c6c8946c8cb9
leeno <leeno@LAPTOP-81IK55DV>
2024-05-23 13:20:06 +0900

    init

---
+++ sbuddy/src/main/java/com/sbuddy/web/SbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.sbuddy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbuddyApplication.class, args);
+	}
+
+}
---
+++ sbuddy/src/test/java/com/sbuddy/web/SbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbuddy.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
