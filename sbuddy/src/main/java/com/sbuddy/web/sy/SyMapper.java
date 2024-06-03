ca53ef2540ae61bf8bbc24ba2d30a04116da63ae
tmddus2123 <tmddus2123@gmail.com>
2024-06-04 00:01:51 +0900

    db 연결

--- sbuddy/src/main/java/com/sbuddy/web/sy/SyMapper.java
+++ sbuddy/src/main/java/com/sbuddy/web/sy/SyMapper.java
@@ -1,8 +1,10 @@
 package com.sbuddy.web.sy;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface SyMapper {
-	
+	public Map<String, Object> getMember();
 }
