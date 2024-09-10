e95a60c2799d6ea22f9136d6b499a9d16daace26
tmddus2123 <tmddus2123@gmail.com>
2024-09-10 15:16:37 +0900

    1) 첫 로그인 시 키워드 등록 검사 로직 추가

--- sbuddy/src/main/java/com/sbuddy/web/member/MemberMapper.java
+++ sbuddy/src/main/java/com/sbuddy/web/member/MemberMapper.java
@@ -18,6 +18,8 @@ public interface MemberMapper {
 	// 임시 비민번호 변경
 	public int changePassword(Map<String, Object> param);
 	
+	// 첫 로그인 시 키워드가 등록되어있는지 검사
+	public int getMemberKeywordCount(Map<String, Object> param);
 	// 첫 로그인 시 회원 키워드 등록
 	public int insertMemberKeyword(Map<String, Object> param);
 }
--- sbuddy/src/main/java/com/sbuddy/web/vo/ResponseCode.java
+++ sbuddy/src/main/java/com/sbuddy/web/vo/ResponseCode.java
@@ -18,7 +18,8 @@ public enum ResponseCode {
 	
 	NOT_EXISTS_MEMBER("1001", HttpStatus.OK, "NOT_EXISTS_MEMBER"),
 	
-	
+	// 첫 로그인 시 키워드 등록할 때 이미 키워드가 등록되어있는 경우
+	ALREADY_CHOICE_KEYWORD("1002", HttpStatus.OK, "ALREADY_CHOICE_KEYWORD"),
 	
 	NOT_EXISTS_POST("2001", HttpStatus.OK, "NOT_EXISTS_POST"),
 	ALREADY_POST_LIKE("2002", HttpStatus.OK, "ALREADY_POST_LIKE"),
@@ -26,6 +27,7 @@ public enum ResponseCode {
 	NOT_EXISTS_POST_LIKE("2003", HttpStatus.OK, "NOT_EXISTS_POST_LIKE"),
 	
 	SEND_FAIL_MESSAGE("5001", HttpStatus.OK, "SEND_FAIL_MESSAGE"),
+	
 	
 	
 	// token에 member_idx 를 찾을 수 없는 경우
